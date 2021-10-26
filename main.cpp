5046f1a0eea5c759ea0c882a7ea0ffc77840559f
Samuel Lin <samuelddlin@gmail.com>
2021-10-25 18:44:03 -0700

    initial commit: bio

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	cout << "Hello!" << endl;
+	cout << "Name: Ying-Jia(Samuel) Lin" << endl;
+	cout << "Major: Computer Science" << endl;
+	cout << "Year: Freshman, Class of 2025" << endl;
+	cout << "Fun Fact: I was born in Scotland, but raised in Canada" << endl;
+
+	return 0;
+}
