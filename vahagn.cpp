f59cada5a4ecbe3753faf587c7c08806416114a6
Vahagn Tovmasian <vahagntovmasian@gmail.com>
2021-10-25 18:58:49 -0700

    Create vahagn.cpp

---
+++ vahagn.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, my name is Vahagn." << endl;
+    cout << "I am a 3d year Computer Engineer." << endl;
+    cout << "I am running this Hacktoberfest Workshop." << endl;
+    
+}
